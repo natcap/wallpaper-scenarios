e97282cd4ea79036314dbb034a0c650d70db7c64
Rich Sharp <richpsharp@gmail.com>
2020-09-01 15:40:48 -0700

    creating the parcel mask

--- wallpaper_raster.py
+++ wallpaper_raster.py
@@ -19,7 +19,7 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 
 
-def makedirs(dir_path):
+def _makedirs(dir_path):
     """Make a directory at `dir_path`."""
     try:
         os.makedirs(dir_path)
@@ -27,14 +27,78 @@ def makedirs(dir_path):
         pass
 
 
-def get_vector_fields(vector_path, field_name):
+def _get_vector_fields(vector_path, field_name):
     """Return list of unique values in `field_name` in `vector_path`."""
     vector = gdal.OpenEx(vector_path, gdal.OF_VECTOR)
     layer = vector.GetLayer()
     return {feature.GetField(field_name) for feature in layer}
 
 
-if __name__ == '__main__':
+def _extract_intersecting_array_from_raster(
+        feature, feature_projection_wkt, raster_path):
+    """Extract numpy array from the raster that intersects with the feature.
+
+    Args:
+        feature (ogr.Feature): feature to use as a georeference to extract
+            the array.
+        feature_projection_wkt (str): a WKT form of the feature SRS.
+        raster_path (str): path to raster to extract the intersecting
+            bounding box from.
+
+    """
+    raster_info = pygeoprocessing.get_raster_info(raster_path)
+    raster_info = pygeoprocessing.get_raster_info(raster_path)
+    raster = gdal.OpenEx(raster_path, gdal.OF_RASTER)
+    raster_gt_inv = gdal.InvGeoTransform(raster_info['geotransform'])
+
+    # get the boundary of the feature in non-interleaved format,
+    # .GetEnvelope returns in interleaved i.e.
+    # (x_min, x_max, y_min, y_max) to
+    # (x_min, y_min, x_max, y_max)
+    scenario_boundary = [
+        feature.GetGeometryRef().GetEnvelope()[i]
+        for i in [0, 2, 1, 3]]
+    scenario_bb = pygeoprocessing.transform_bounding_box(
+        scenario_boundary,
+        feature_projection_wkt,
+        raster_info['projection_wkt'])
+    LOGGER.debug(f'{scenario_boundary} {scenario_bb}')
+
+    x_min, y_min = [
+        int(v) for v in gdal.ApplyGeoTransform(
+            raster_gt_inv, scenario_bb[0], scenario_bb[3])]
+    x_max, y_max = [
+        int(v) for v in gdal.ApplyGeoTransform(
+            raster_gt_inv, scenario_bb[2], scenario_bb[1])]
+
+    array = raster.ReadAsArray(
+        xoff=x_min, yoff=y_min, xsize=x_max-x_min, ysize=y_max-y_min)
+    return array
+
+
+def _create_vector_mask(
+        base_raster_path, vector_path, target_mask_raster_path):
+    """Create a nodata/1 mask of `raster` that is 1 where vector covers.
+
+    Args:
+        base_raster_path (str): path to base raster to use as size/projection
+            for `target_mask_raster_path`.
+        vector_path (str): path to vector to use as mask for base
+        target_mask_raster_path (str): file that is created that is nodata
+            by default and 1 where vector intersects.
+
+    Returns:
+        None.
+
+    """
+    pygeoprocessing.new_raster_from_base(
+        base_raster_path, target_mask_raster_path, gdal.GDT_Byte, [0])
+    pygeoprocessing.rasterize(
+        vector_path, target_mask_raster_path, burn_values=[1])
+
+
+def main():
+    """Entry point."""
     parser = argparse.ArgumentParser(
         description='Tile a raster by another raster with a mask')
     parser.add_argument(
@@ -55,53 +119,38 @@ if __name__ == '__main__':
         help='target directory')
     args = parser.parse_args()
 
-    makedirs(args.workspace_dir)
+    _makedirs(args.workspace_dir)
 
     scenario_vector_info = pygeoprocessing.get_vector_info(
         args.scenarios_vector_path)
 
     for raster_path in args.raster_path_list:
-        basename = os.path.splitext(os.path.basename(raster_path))
+        basename = os.path.splitext(os.path.basename(raster_path))[0]
         churn_dir = os.path.join(args.workspace_dir, f'churn')
-        makedirs(churn_dir)
-        raster_info = pygeoprocessing.get_raster_info(raster_path)
-        raster = gdal.OpenEx(raster_path, gdal.OF_RASTER)
-        raster_gt_inv = gdal.InvGeoTransform(raster_info['geotransform'])
+        _makedirs(churn_dir)
 
         scenario_vector = gdal.OpenEx(
             args.scenarios_vector_path, gdal.OF_VECTOR)
         scenario_layer = scenario_vector.GetLayer()
+
+        parcel_mask_raster_path = os.path.join(
+            churn_dir, f'parcel_mask_{basename}.tif')
+
+        # Create a vector mask
+        pygeoprocessing.new_raster_from_base(
+            raster_path, parcel_mask_raster_path, gdal.GDT_Byte, [0])
+        pygeoprocessing.rasterize(
+            args.parcels_vector_path, parcel_mask_raster_path, burn_values=[1])
+
         for scenario_feature in scenario_layer:
-            # get the boundary of the feature in non-interleaved format,
-            # .GetEnvelope returns in interleaved i.e.
-            # (x_min, x_max, y_min, y_max) to
-            # (x_min, y_min, x_max, y_max)
-            scenario_boundary = [
-                scenario_feature.GetGeometryRef().GetEnvelope()[i]
-                for i in [0, 2, 1, 3]]
-            scenario_bb = pygeoprocessing.transform_bounding_box(
-                scenario_boundary,
-                scenario_vector_info['projection_wkt'],
-                raster_info['projection_wkt'])
-            LOGGER.debug(f'{scenario_boundary} {scenario_bb}')
-
-            x_min, y_min = [
-                int(v) for v in gdal.ApplyGeoTransform(
-                    raster_gt_inv, scenario_bb[0], scenario_bb[3])]
-            x_max, y_max = [
-                int(v) for v in gdal.ApplyGeoTransform(
-                    raster_gt_inv, scenario_bb[2], scenario_bb[1])]
-
-            scenario_id = scenario_feature.GetField(args.scenario_id_field)
-            LOGGER.debug(f'{x_min} {y_min} {x_max} {y_max}')
-            scenario_raster_path = os.path.join(
-                churn_dir, f'scenario_{basename}_{scenario_id}.tif')
-            scenario_array = raster.ReadAsArray(
-                xoff=x_min, yoff=y_min, xsize=x_max-x_min, ysize=y_max-y_min)
+            scenario_array = _extract_intersecting_array_from_raster(
+                scenario_feature, scenario_vector_info['projection_wkt'],
+                raster_path)
             LOGGER.debug(scenario_array)
-            pass
 
 
+if __name__ == '__main__':
+    main()
 
 # orkspace = r"E:\NatCap\Golf\Comparison\Data"
 # folder = os.path.join(workspace, r"pol\Data_1m")
